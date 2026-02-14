c5bf794fa6ca7e506cce40ae7565a9c92a7152e7
appcrafting123-eng <appcrafting123@gmail.com>
2026-02-14 18:53:46 +0530

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,6 +11,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 
 INSTALLED_APPS = [
     "django.contrib.admin",
